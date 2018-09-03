2a966dab999caf73a57d9e49cfd0239b26b1cf39
Leo Torres <leo.htor@gmail.com>
2018-09-03 19:59:06 -0400

    Added initial Stripe payment button. Added services cards. Added media queries for cards.

--- src/pages/services.js
+++ src/pages/services.js
@@ -4,6 +4,27 @@ import styled from 'styled-components';
 import Img from 'gatsby-image';
 import Checkout from '../components/checkout';
 
+const mediaSize = {
+  mobileS: '320px',
+  mobileM: '375px',
+  mobileL: '413px',
+  tablet: '768px',
+  laptop: '1082px',
+  laptopL: '1440px',
+  desktop: '2560px'
+};
+
+export const device = {
+  mobileS: `(max-width: ${mediaSize.mobileS})`,
+  mobileM: `(max-width: ${mediaSize.mobileM})`,
+  mobileL: `(max-width: ${mediaSize.mobileL})`,
+  tablet: `(max-width: ${mediaSize.tablet})`,
+  laptop: `(max-width: ${mediaSize.laptop})`,
+  laptopL: `(max-width: ${mediaSize.laptopL})`,
+  desktop: `(max-width: ${mediaSize.desktop})`,
+  desktopL: `(max-width: ${mediaSize.desktop})`
+};
+
 const OuterWrapper = styled.div`
   position: relative;
   overflow: hidden;
@@ -19,7 +40,7 @@ const ContainerWrapper = styled.div`
   padding: 0px 1.0875rem 1.45rem;
   padding-top: 0;
   margin-top: 1.45rem;
-  max-width: 960px;
+  // max-width: 960px;
   min-height: 80vh;
   color: #4d4d4d;
   p {
@@ -36,22 +57,47 @@ const ContainerWrapper = styled.div`
 
 const CardContainer = styled.div`
   display: flex;
+  justify-content: space-between;
   padding: 1.45rem 1.0875rem 1.45rem;
-  // justify-content: space-around:
+
+  @media ${device.laptop} {
+    display: inline-block;
+    justify-content: space-around;
+    padding-left: 1rem;
+  }
+  @media ${device.mobileL} {
+    padding: 0;
+  }
 `;
+
 
 const Card = styled.div`
-  display: flex;
-  justify-content: space-around;
-  margin: 30px;
-  padding-top: 40px;
+  position: relative;
+  margin: 12px;
   text-align: center;
-  border: 2px solid #4d4d4d;
-  border-radius: 40px;
-  max-width: 25rem;
+  overflow: hidden;
+  border-radius: 4px;
+  width: 350px;
+  min-width: 350px;
+  height: 504px;
+  min-height: 304px;
+  box-shadow: 0 27px 55px 0 rgba(0, 0, 0, 0.3), 0 17px 17px 0 rgba(0, 0, 0, 0.15);
+   p {
+     padding: 1rem;
+     font-size: 14px;
+   }
+
+  @media ${device.laptop} {
+    margin: 1rem auto;
+  }
+
+  @media ${device.mobileL} {
+    margin: 1rem auto;
+    // margin-left: 0;
+  }
 `;
 
-console.log({  });
+
 
 
 const Services = ({ data }) => (
@@ -77,11 +123,10 @@ const Services = ({ data }) => (
         position: 'relative',
         // left: 0,
         top: 0,
-        width: '2rem',
-        height: '2rem',
-        opacity: 0.8,
+        // width: '100vw',
+        height: '200px',
       }}
-        sizes={data.card.sizes}/>
+        sizes={data.single.sizes} />
         <p>
           It is my privilege to accompany you in one-on-one on a journey to excavate and heal pain of body and mind, unlock potential, and build toward empowered, integrated embodiment.
           Book Online
@@ -97,11 +142,10 @@ const Services = ({ data }) => (
         position: 'relative',
         // left: 0,
         top: 0,
-        width: '2rem',
-        height: '2rem',
-        opacity: 0.8,
+        // width: '100vw',
+        height: '200px',
       }}
-         sizes={data.card.sizes}/>
+         sizes={data.couple.sizes} />
         <p>
           Couples coaching creates a safe space in which to shine the light of awareness on unaddressed resentment, fear, and avoidance, clarify boundaries, needs and agreements, overcome painful patterns, build or rebuild trust, and co-create a vibrant new relationship vision.
           Book Online
@@ -119,11 +163,10 @@ const Services = ({ data }) => (
         position: 'relative',
         // left: 0,
         top: 0,
-        width: '2rem',
-        height: '2rem',
-        opacity: 0.8,
-      }}
-         sizes={data.card.sizes}/>
+        // width: '350px',
+        height: '200px',
+        }}
+         sizes={data.family.sizes} />
         </div>
 
         <p>
@@ -155,8 +198,18 @@ export const query = graphql`
         title
       }
     }
-    card: imageSharp(id: {regex: "/about-bg/"}) {
-      sizes(maxWidth: 30) {
+    single: imageSharp(id: {regex: "/couple-card/"}) {
+      sizes(maxWidth: 200) {
+        ...GatsbyImageSharpSizes
+      }
+    }
+    couple: imageSharp(id: {regex: "/couple-card/"}) {
+      sizes(maxWidth: 200) {
+        ...GatsbyImageSharpSizes
+      }
+    }
+    family: imageSharp(id: {regex: "/couple-card/"}) {
+      sizes(maxWidth: 200) {
         ...GatsbyImageSharpSizes
       }
     }
