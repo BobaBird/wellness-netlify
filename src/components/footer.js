696e163d1e9da417780078716e105c734badce85
Leo Torres <leo.htor@gmail.com>
2018-09-06 13:44:30 -0400

    Updated Footer styles and columns.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,18 +1,40 @@
 import React from 'react';
 import Link from 'gatsby-link';
+import styled from 'styled-components';
+
+const FooterWrapper = styled.div`
+  display: flex;
+  background-color: #282C33;
+  position: static;
+  width: 100vw;
+  bottom: 0;
+  left: 0;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+
+const FooterContainer = styled.div`
+
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+  height: 180px;
+  color: gray;
+`;
 
 const Footer = () => (
-  <div style={{
-    background: '#282C33', position: 'static', width: '100vw', bottom: '0', left: '0',
-  }}
-  >
-    <div style={{
-      margin: '0 auto', maxWidth: 960, padding: '1.45rem 1.0875rem', color: '#F6950A', height: '180px',
-    }}
-    >
-      <h1>Footer</h1>
-    </div>
-  </div>
+  <FooterWrapper>
+    <FooterContainer>
+      <h2>Other Info</h2>
+    </FooterContainer>
+    <FooterContainer>
+      <h2>Social Media</h2>
+    </FooterContainer>
+    <FooterContainer>
+      <h2>Webmaster</h2>
+    </FooterContainer>
+  </FooterWrapper>
 );
 
 export default Footer;
