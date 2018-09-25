6a1ba513b068cbdea7ffb695a9dfdf68e8275a90
Leo Torres <leohtor@gmail.com>
2018-09-25 09:04:20 -0700

    Required form fields.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -164,14 +164,14 @@ const Contact = ({ data }) => (
           <form name="contact" method="post" data-netlify="true" netlify-honeypot="bot-field">
               <input type="hidden" name="form-name" value="contact" />
               <p>
-                <label>Name: <input type="text" name="name" /></label>   
+                <label>Name: <input type="text" name="name" required /></label>   
               </p>
               <p>
-                <label>Email: <input type="email" name="email" /></label>
+                <label>Email: <input type="email" name="email" required /></label>
               </p>
               
               <p>
-                <label>Message: <textarea name="message" ></textarea></label>
+                <label>Message: <textarea name="message" required ></textarea></label>
               </p>
               <p>
                 <button type="submit">Send</button>
